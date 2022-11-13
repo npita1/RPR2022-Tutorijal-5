089d6485e08fc7afa309c9d78cf068cf2955da33
npita1 <npita1@etf.unsa.ba>
2022-11-13 18:14:24 +0100

    Napravljen interfejs AutoPlan i klasa Auto sa neimplementiranim metodama

---
+++ t5-z1/src/main/java/ba/unsa/etf/rpr/Auto.java
@@ -0,0 +1,29 @@
+package ba.unsa.etf.rpr;
+
+public class Auto implements AutoPlan{
+
+    private Boolean krov;
+    private Integer brojVrata;
+    private String mjenjac;
+    private Integer brojBrzina;
+
+    @Override
+    public void postaviKrov(Boolean imakrov) {
+
+    }
+
+    @Override
+    public void postaviVrata(Integer vrata) {
+
+    }
+
+    @Override
+    public void postaviMjenjac(String mjenjac) {
+
+    }
+
+    @Override
+    public void postaviBrojBrzina(Integer brzine) {
+
+    }
+}
---
+++ t5-z1/src/main/java/ba/unsa/etf/rpr/AutoPlan.java
@@ -0,0 +1,10 @@
+package ba.unsa.etf.rpr;
+
+public interface AutoPlan {
+
+    void postaviKrov(Boolean imakrov);
+    void postaviVrata(Integer vrata);
+    void postaviMjenjac(String mjenjac);
+    void postaviBrojBrzina(Integer brzine);
+
+}
