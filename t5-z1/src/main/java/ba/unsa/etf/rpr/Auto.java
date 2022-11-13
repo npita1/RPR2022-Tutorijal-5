65feadef7e9db4f6f33aa6f5c726c902257bcc93
npita1 <npita1@etf.unsa.ba>
2022-11-13 18:19:17 +0100

    Implementirane metode i dodani getteri u klasu Auto

--- t5-z1/src/main/java/ba/unsa/etf/rpr/Auto.java
+++ t5-z1/src/main/java/ba/unsa/etf/rpr/Auto.java
@@ -4,26 +4,42 @@ public class Auto implements AutoPlan{
 
     private Boolean krov;
     private Integer brojVrata;
-    private String mjenjac;
+    private String vrstaMjenjaca;
     private Integer brojBrzina;
 
     @Override
     public void postaviKrov(Boolean imakrov) {
-
+        this.krov = imakrov;
     }
 
     @Override
     public void postaviVrata(Integer vrata) {
-
+        this.brojVrata = vrata;
     }
 
     @Override
     public void postaviMjenjac(String mjenjac) {
-
+        this.vrstaMjenjaca = mjenjac;
     }
 
     @Override
     public void postaviBrojBrzina(Integer brzine) {
+        this.brojBrzina = brzine;
+    }
 
+    public Boolean getKrov() {
+        return krov;
+    }
+
+    public Integer getBrojVrata() {
+        return brojVrata;
+    }
+
+    public String getVrstaMjenjaca() {
+        return vrstaMjenjaca;
+    }
+
+    public Integer getBrojBrzina() {
+        return brojBrzina;
     }
 }
