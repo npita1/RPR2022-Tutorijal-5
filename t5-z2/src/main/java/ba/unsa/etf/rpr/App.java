6ace4f8565b5e137c7575eda6502f388323771da
npita1 <npita1@etf.unsa.ba>
2022-11-14 14:41:36 +0100

    Zavrsen drugi zadatak, Factory pattern implementacija

--- t5-z2/src/main/java/ba/unsa/etf/rpr/App.java
+++ t5-z2/src/main/java/ba/unsa/etf/rpr/App.java
@@ -5,6 +5,9 @@ public class App
     public static void main( String[] args )
     {
 
-        System.out.println( "Hello World!" );
+        PizzaFactory pizzaMajstor = new PizzaFactory();
+        Pizza mojaPizza = pizzaMajstor.naruciPizzu("Margarita");
+        mojaPizza.napraviPicu();
+
     }
 }
